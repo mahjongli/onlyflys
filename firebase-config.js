4d3a717ca0a220abd71418dab31bc7be3f4bb730
mahjongli <134165084+mahjongli@users.noreply.github.com>
2026-09-16 01:23:26 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -11,12 +11,12 @@
 // ---------------------------------------------------------------
 
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyDVb5jt2KvZkZXldkzA_D20zUrOJXyxR_E",
+  authDomain: "onlyfly-21a17.firebaseapp.com",
+  projectId: "onlyfly-21a17",
+  storageBucket: "onlyfly-21a17.firebasestorage.ap",
+  messagingSenderId: "501420678853",
+  appId: "1:501420678853:web:ddc0a7abf0584b344bdaac"
 };
 
 // Everything lives under this room. Change it to run a second, separate
